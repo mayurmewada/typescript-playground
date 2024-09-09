b39d7de73ff57e8f7f7e361c7d41966c6f05f6b2
mayurmewada <mayurmewada00@gmail.com>
2024-09-09 23:01:41 +0530

    new: accept type and return type

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
 let age = 20;
+const render = (parameters) => {
+    console.log(parameters);
+};
+const funtionCheckOddEven = (num) => {
+    return num % 2 === 0;
+};
+console.log(funtionCheckOddEven(2));
+console.log(funtionCheckOddEven(3));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 let age: number = 20;
 
-
 // any type
 
 const render = (parameters: any) => {
-    console.log(parameters)
-}
+    console.log(parameters);
+};
+
+const funtionCheckOddEven = (num: number): boolean => {
+    return num % 2 === 0;
+};
+
+console.log(funtionCheckOddEven(2));
+console.log(funtionCheckOddEven(3));
